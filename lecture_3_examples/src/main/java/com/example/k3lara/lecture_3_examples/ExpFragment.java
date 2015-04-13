89ec410c65d04b75cd6f27846baaa7612c8848a7
Lars <K3LARRA@HOTMAIL.COM>
2015-04-13 13:56:42 +0200

    From lecture

--- lecture_3_examples/src/main/java/com/example/k3lara/lecture_3_examples/ExpFragment.java
+++ lecture_3_examples/src/main/java/com/example/k3lara/lecture_3_examples/ExpFragment.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ExpandableListView;
+import android.widget.Spinner;
 
 import com.example.k3lara.lecture_3_examples.xmlparser.Parser;
 import com.example.k3lara.lecture_3_examples.xmlparser.ScheduleItem;
@@ -19,7 +22,7 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ExpFragment extends Fragment {
+public class ExpFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
     private ArrayList<ScheduleItem> myItems = new ArrayList<ScheduleItem>();
 private MyAdapter myAdapter;
@@ -27,6 +30,11 @@ private MyAdapter myAdapter;
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,13 +45,46 @@ private MyAdapter myAdapter;
         ExpandableListView ev = (ExpandableListView) v.findViewById(R.id.expandableListView);
         myAdapter = new MyAdapter(getActivity(),myItems);
         ev.setAdapter(myAdapter);
-
-        MyAsyncTask myAsyncTask = new MyAsyncTask();
-        myAsyncTask.execute("KD323B-20151-K3522");
-
+        //MyAsyncTask myAsyncTask = new MyAsyncTask();
+        //myAsyncTask.execute("KD323B-20151-K3522");
+        Spinner sp = (Spinner)v.findViewById(R.id.spinner);
+        sp.setOnItemSelectedListener(this);
         return v;
     }
 
+    ///Listens to meny selection.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Log.i("ExpFragment", "MenuSelection. " + id);
+        if (id == R.id.refresh) {
+            Spinner sp = (Spinner)getView().findViewById(R.id.spinner);
+            int i = sp.getSelectedItemPosition();
+            String[] sa = getActivity().getResources().getStringArray(R.array.courses);
+            String course = sa[i];
+            MyAsyncTask myAsyncTask = new MyAsyncTask();
+            myAsyncTask.execute(course);
+            return true;
+        }else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Listens to the spinner
+  @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String[] courses = getActivity().getResources().getStringArray(R.array.courses);
+            String course = courses[position];
+            Log.i("ExpFragment", "Course. " + course);
+            MyAsyncTask myAsyncTask = new MyAsyncTask();
+            myAsyncTask.execute(course);
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    //And the thread
     private class MyAsyncTask extends AsyncTask<String,Void,Void>{
 
         @Override
